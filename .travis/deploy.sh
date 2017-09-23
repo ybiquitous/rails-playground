2e1e3c44dd32ed9ab0f72cfd2d2232038e981995
ybiquitous <ybiquitous@gmail.com>
2017-09-23 17:55:07 +0900

    setup database before build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-bundle exec rails diagram:all
+bin/rails diagram:all
 git add doc
 git commit -m 'update class diagrams'
 git push --force --quiet "https://${GH_TOKEN}@github.com/ybiquitous/rails-playground.git" >/dev/null 2>&1
