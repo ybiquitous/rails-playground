509e63d7cc83ef17036191ae2df6e5e736707245
ybiquitous <ybiquitous@gmail.com>
2017-09-23 18:38:06 +0900

    fix missing deploy branch

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,4 @@ set -ev
 bin/rails diagram:all
 git add doc
 git commit -m 'update class diagrams'
-git show
-git push --force --quiet "https://${GH_TOKEN}@github.com/ybiquitous/rails-playground.git"
+git push --force --quiet "https://${GH_TOKEN}@github.com/ybiquitous/rails-playground.git" "HEAD:${TRAVIS_BRANCH}"
