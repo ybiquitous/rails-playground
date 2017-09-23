b9f403f643f4d37cd9537da0a70afbb56aa3d2a3
ybiquitous <ybiquitous@gmail.com>
2017-09-23 18:27:02 +0900

    Revert "deploy -> after_success"
    This reverts commit f7c1a1c6b3b10ffc6aad453737bad558eb0addc4.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,4 +4,5 @@ set -ev
 bin/rails diagram:all
 git add doc
 git commit -m 'update class diagrams'
+echo "${TRAVIS_BRANCH}"
 git push --force --quiet "https://${GH_TOKEN}@github.com/ybiquitous/rails-playground.git" >/dev/null 2>&1
