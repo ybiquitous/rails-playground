c24b3b805e43d02dd70e4e70c51e5f5b0882c454
ybiquitous <ybiquitous@gmail.com>
2017-09-24 17:42:05 +0900

    auto generate class diagrams

---
+++ .travis/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ev
+
+bin/rails diagram:all
+git add doc
+
+cat <<MSG | git commit --file -
+update class diagrams
+
+[skip ci]
+MSG
+
+git push --force --quiet \
+    "https://${GH_TOKEN}@github.com/ybiquitous/rails-playground.git" \
+    "HEAD:${TRAVIS_BRANCH}"
