aadee69429512b3b7ed676572f5e9e2eae33ac5a
ybiquitous <ybiquitous@gmail.com>
2017-11-10 01:42:39 +0900

    feat: add player pages

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class PlayersController < ApplicationController
+  def index
+    @players = Player.all
+  end
+
+  def show
+    @player = find_player
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def edit
+    @player = find_player
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to @player, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  def update
+    @player = find_player
+    if @player.update(player_params)
+      redirect_to @player, notice: t('.success')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @player = find_player
+    @player.destroy
+    redirect_to players_url, notice: t('.success')
+  end
+
+  private
+
+  def find_player
+    Player.find(params[:id])
+  end
+
+  def player_params
+    params.require(:player).permit(:name, :status)
+  end
+end
