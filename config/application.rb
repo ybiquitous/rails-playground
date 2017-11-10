bd7762dd70b532543c8c02847e29e7f3f2c3e95f
ybiquitous <ybiquitous@gmail.com>
2017-11-10 14:14:21 +0900

    fix: raise error when unpermitted parameters are sent

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module RailsPlayground
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
