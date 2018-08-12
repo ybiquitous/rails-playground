7c2778cd9d486ac5e0838f27aa2cd4fcbbd2f022
Masafumi Koba <ybiquitous@gmail.com>
2018-08-12 14:43:01 +0900

    fixup! fixup! fixup! fixup! fixup! test: introduce system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,7 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  # > To use Chrome in the container-based environment, pass the --no-sandbox flag to the chrome executable.
+  # See https://docs.travis-ci.com/user/chrome#sandboxing
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: { args: %w[--no-sandbox] }
 end
